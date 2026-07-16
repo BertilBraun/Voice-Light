22cb8a3fc932358e9fcc3b63e465b48a0cb5453b
Bertil Braun <bertil.braun.private@gmail.com>
2026-07-16 16:16:29 +0200

    Mark synchronization review block through PMT 144

--- app/local/synchronization_review/calibration.py
+++ app/local/synchronization_review/calibration.py
@@ -10,12 +10,25 @@ class ReviewedAlignment:
 
 
 REVIEWED_ALIGNMENTS = (
+    ReviewedAlignment(external_id="pmt_284", speaker2_shift_seconds=-6.8),
+    ReviewedAlignment(external_id="pmt_205", speaker2_shift_seconds=-10.0),
+    ReviewedAlignment(external_id="pmt_236", speaker2_shift_seconds=-4.8),
+    ReviewedAlignment(external_id="pmt_008", speaker2_shift_seconds=-4.2),
+    ReviewedAlignment(external_id="pmt_132", speaker2_shift_seconds=-3.2),
+    ReviewedAlignment(external_id="pmt_161", speaker2_shift_seconds=-3.2),
+    ReviewedAlignment(external_id="pmt_226", speaker2_shift_seconds=-2.5),
+    ReviewedAlignment(external_id="pmt_180", speaker2_shift_seconds=-3.6),
+    ReviewedAlignment(external_id="pmt_315", speaker2_shift_seconds=-2.6),
     ReviewedAlignment(external_id="pmt_095", speaker2_shift_seconds=-2.9),
     ReviewedAlignment(external_id="pmt_192", speaker2_shift_seconds=-4.0),
-    ReviewedAlignment(external_id="pmt_205", speaker2_shift_seconds=-10.0),
-    ReviewedAlignment(external_id="pmt_226", speaker2_shift_seconds=-2.5),
-    ReviewedAlignment(external_id="pmt_236", speaker2_shift_seconds=-4.8),
-    ReviewedAlignment(external_id="pmt_284", speaker2_shift_seconds=-6.8),
+    ReviewedAlignment(external_id="pmt_256", speaker2_shift_seconds=-3.6),
+    ReviewedAlignment(external_id="pmt_217", speaker2_shift_seconds=-4.2),
+    ReviewedAlignment(external_id="pmt_237", speaker2_shift_seconds=-2.4),
+    ReviewedAlignment(external_id="pmt_087", speaker2_shift_seconds=-1.6),
+    ReviewedAlignment(external_id="pmt_225", speaker2_shift_seconds=-2.8),
+    ReviewedAlignment(external_id="pmt_219", speaker2_shift_seconds=-1.4),
+    ReviewedAlignment(external_id="pmt_318", speaker2_shift_seconds=-0.4),
+    ReviewedAlignment(external_id="pmt_144", speaker2_shift_seconds=-2.0),
 )
 
 
